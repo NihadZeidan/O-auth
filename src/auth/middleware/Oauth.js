38683273f5d110b87f7e1610b290c7ce86ab4af6
mohdAzzam <alazzamhmode@gmail.com>
2021-05-19 17:52:57 +0300

    asas

--- src/auth/middleware/Oauth.js
+++ src/auth/middleware/Oauth.js
@@ -42,7 +42,7 @@ async function exchangeCodeWithToken(code) {
             code: code,
             client_id: CLIENT_ID,
             client_secret: CLIENT_SECRET,
-            redirect_uri: 'https://auth-team-ltuc.herokuapp.com/api/auth',
+            redirect_uri: 'https://o-auth-team.herokuapp.com/',
             grant_type: 'authorization_code'
 
         });
